7c12993b7c1ef90fb7fa8b42379e51db53a549a8
yunha-cha <helena0228c@gmail.com>
2024-06-09 23:48:26 +0900

    add comment

---
+++ src/main/java/com/yunha/boardproject/board/entity/Comment.java
@@ -0,0 +1,32 @@
+//package com.yunha.boardproject.board.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//import java.util.Date;
+//
+//@Entity
+//@Table(name = "comment")
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//public class Comment {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "comment_cpde")
+//    private Long commentCode;
+//
+//    @Column(name = "comment_content")
+//    private String commentContent;
+//
+//    @Column(name = "comment_writer")
+//    private String commentWriter;
+//
+//    @Column(name = "comment_date")
+//    private Date commentDate;
+//
+//    
+//}
